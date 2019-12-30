6089067eac743cfb52cb7de2807025fe2d4e2ff9
Daniel Gregson <kornfreakuk@hotmail.com>
2019-12-30 14:33:51 +0000

    Implement basic turning and state for turning with accompanying tests

--- src/DG.MarsRover/MarsRover.cs
+++ src/DG.MarsRover/MarsRover.cs
@@ -1,15 +1,47 @@
-﻿namespace DG.MarsRover
+﻿using System.Linq;
+
+namespace DG.MarsRover
 {
+    public enum CompassDirection
+    {
+        N = 1,
+        E = 2,
+        S = 3,
+        W = 4
+    }
+
     public class MarsRover
     {
+        private CompassDirection _compassDirection;
         public MarsRover()
         {
-            
+            _compassDirection = CompassDirection.N;
         }
 
-        public string Execute(string command)
+        public string Execute(string commandString)
         {
-            return "0:0:N";
+            var commands = commandString.Select(c => c.ToString());
+            foreach (var command in commands)
+            {
+                AlterCompassDirection(command);
+            }
+            return $"0:0:{_compassDirection}";
+        }
+
+        private void AlterCompassDirection(string command)
+        {
+            if (command == "R")
+            {
+                _compassDirection += 1;
+                if ((int)_compassDirection == 5)
+                    _compassDirection = CompassDirection.N;
+            }
+            else if (command == "L")
+            {
+                _compassDirection -= 1;
+                if ((int)_compassDirection == 0)
+                    _compassDirection = CompassDirection.W;
+            }
         }
     }
 }
--- tests/DG.MarsRover.Tests/MarsRoverTests.cs
+++ tests/DG.MarsRover.Tests/MarsRoverTests.cs
@@ -16,5 +16,26 @@ namespace DG.MarsRover.Tests
             //Assert
             Assert.That(result, Is.EqualTo("0:0:N"));
         }
+
+        [Test]
+        [TestCase("L", "0:0:W")]
+        [TestCase("LL", "0:0:S")]
+        [TestCase("LLL", "0:0:E")]
+        [TestCase("LLLL", "0:0:N")]
+        [TestCase("R", "0:0:E")]
+        [TestCase("RR", "0:0:S")]
+        [TestCase("RRR", "0:0:W")]
+        [TestCase("RRRR", "0:0:N")]
+        public void DirectionalCommand_ReturnsCompassDirection(string input, string expectedOutput)
+        {
+            //Arrange
+            var rover = new MarsRover();
+
+            //Act
+            var result = rover.Execute(input);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
     }
 }
